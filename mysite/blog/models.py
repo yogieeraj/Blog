8a477a2883ecb442619b730b3d95a9974a995d26
Yogirajsinh Rathod <yogirajsinh77752@gmail.com>
2020-03-11 17:54:54 -0400

    Add files via upload

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -7,6 +7,7 @@ from django.urls import reverse
 # Password: testpassword
 
 # Create your models here.
+
 class Post(models.Model):
     author = models.ForeignKey('auth.User',on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
